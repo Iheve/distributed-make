5dd55c931693c0e1b7adb0f71189df5723b3921d
Nicolas Vignes <nicolas.vignes@ensimag.grenoble-inp.fr>
2015-01-07 16:41:08 +0100

    Fix #7 Read parameters from the command line, the parameter is optionnal and the default value port number is 4242

--- listener/listener.go
+++ listener/listener.go
@@ -6,13 +6,24 @@ import (
 	"net"
 	"net/http"
 	"net/rpc"
+	"os"
 )
 
 func main() {
+	var port string
+
 	worker := new(Worker)
 	rpc.Register(worker)
 	rpc.HandleHTTP()
-	l, e := net.Listen("tcp", ":1234")
+
+	// Check the args, use default port (= 4242)
+	if len(os.Args) != 2 {
+		port = ":4242"
+	} else {
+		port = ":" + os.Args[1]
+	}
+
+	l, e := net.Listen("tcp", port)
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
