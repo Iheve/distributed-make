834b6a3dae1a321aad56bfcd5df80067cdc75ac3
Thibaut Coutelou <Thibaut.Coutelou@ensimag.grenoble-inp.fr>
2015-01-13 16:31:11 +0100

    Always use bash to execute a command

--- worker/worker.go
+++ worker/worker.go
@@ -1,9 +1,7 @@
 package worker
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -29,60 +27,10 @@ type Response struct {
 }
 
 func execute(cmd, dir string, env []string) (outCmd []byte, err error) {
-	if strings.ContainsAny(cmd, ";><$`&") {
-		c := exec.Command("bash", "-c", cmd)
-		c.Dir = dir
-		c.Env = env
-		return c.CombinedOutput()
-	}
-	var output bytes.Buffer
-	var cmdSplit = strings.Split(cmd, "|")
-	var cmds []*exec.Cmd
-	var pipes = make([]*io.PipeWriter, len(cmdSplit)-1)
-	var i int
-
-	for _, oneCmd := range cmdSplit {
-		oneCmd := strings.TrimSpace(oneCmd)
-		args := strings.Split(oneCmd, " ")
-		c := exec.Command(args[0], args[1:]...)
-		c.Dir = dir
-		c.Env = env
-		cmds = append(cmds, c)
-	}
-
-	for i = 0; i < len(cmds)-1; i++ {
-		in, out := io.Pipe()
-		cmds[i].Stdout = out
-		cmds[i+1].Stdin = in
-		pipes[i] = out
-	}
-	cmds[i].Stdout = &output
-	cmds[i].Stderr = &output
-
-	if err := call(cmds, pipes); err != nil {
-		log.Printf("Command failed with error: %v", err)
-	}
-	return output.Bytes(), err
-}
-
-func call(cmds []*exec.Cmd, pipes []*io.PipeWriter) (err error) {
-	if cmds[0].Process == nil {
-		if err = cmds[0].Start(); err != nil {
-			return err
-		}
-	}
-	if len(cmds) > 1 {
-		if err = cmds[1].Start(); err != nil {
-			return err
-		}
-		defer func() {
-			if err == nil {
-				pipes[0].Close()
-				err = call(cmds[1:], pipes[1:])
-			}
-		}()
-	}
-	return cmds[0].Wait()
+	c := exec.Command("bash", "-c", cmd)
+	c.Dir = dir
+	c.Env = env
+	return c.CombinedOutput()
 }
 
 type Worker int
