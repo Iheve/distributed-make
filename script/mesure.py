d9180c65dcfa050ed082fc96fb9db45e56e32dcb
Nicolas Vignes <nicolas.vignes@ensimag.grenoble-inp.fr>
2015-01-21 14:39:33 +0100

    Improve calculation mesure

--- script/mesure.py
+++ script/mesure.py
@@ -2,11 +2,15 @@
 
 import os
 import subprocess
+from collections import defaultdict
 
 #nbthread = [1,2,4,8,16,32]
+nbmachine = [1]
 nbthread = [1,2,3,4]
-hosts = ["ensipc101", "ensipc103", "ensipc102", "ensipc104", "ensipc105", "ensipc106",
-			"ensipc107", "ensipc108", "ensipc109",  "ensipc110", "ensipc111", "ensipc112"]
+#hosts = ["ensipc101", "ensipc103", "ensipc102", "ensipc104", "ensipc105", "ensipc106",
+#			"ensipc107", "ensipc108", "ensipc109",  "ensipc110", "ensipc111", "ensipc112"]
+
+hosts = ["localhost"]
 start = 101
 fname = "mesures.csv"
 
@@ -16,50 +20,64 @@ script_repository = os.path.dirname(os.path.realpath(__file__))
 cmd_start = "./start.sh"
 cmd_stop = "./stop.sh"
 
+result = defaultdict(list)
 
-for i in nbthread:
-	list_hosts = hosts[:i]
+def moyenne(dict):
+	result = []
+	for k, v in dict:
+		result.append([k,min(v)])
 
-	print("Calculating for nb thread : " + str(i))
-	print("Will using computers : " + str(list_hosts))
+	return result
+
+for y in nbmachine:
+	list_hosts = hosts[:y]
+	print("Calculating with " + str(y) + " computers")
+	print("Will use computers : " + str(list_hosts))
 
 	file_out = open("/tmp/hosts", "wb")
 	for c in list_hosts:
 		file_out.write(c)
 		file_out.write("\n")
 	file_out.close()
-	
-	# Start.sh
-	#full_cmd_start = cmd_start + " -a " + str(start) + " -b " + str(start + i - 1)
-	#print(full_cmd_start)
 
-	#p = subprocess.Popen(full_cmd_start , shell=True, stdout=subprocess.PIPE, 
-	#							stderr=subprocess.PIPE, cwd=script_repository)
-	#p.wait()
+	for i in nbthread:
+		print("Calculating for nb thread : " + str(i*y))
 
-	for j in range(1,5):
-		print("Calculous for " + str(j))
-		# Make clean
-		print("make clean")
+		for j in range(1,11):
+			print("Calculous for " + str(j))
 
-		p = subprocess.Popen("make clean" , shell=True, stdout=subprocess.PIPE, 
-									stderr=subprocess.PIPE, cwd=fmakefile)
-		p.wait()
+			# Make clean
+			print("make clean")
 
-		# Launch the client
-		full_cmd_client = "export TIMEFORMAT=%E; time $GOPATH/bin/client --hostfile /tmp/hosts --makefile Makefile-verysmall"
-		
-		print(full_cmd_client)
+			p = subprocess.Popen("make clean" , shell=True, stdout=subprocess.PIPE, 
+										stderr=subprocess.PIPE, cwd=fmakefile)
+			p.wait()
 
-		p = subprocess.Popen(full_cmd_client , shell=True, stdout=subprocess.PIPE, 
-									stderr=subprocess.PIPE, cwd=fmakefile)
-		out, err = p.communicate()
+			# Launch the client
+			full_cmd_client = "export TIMEFORMAT=%E; time $GOPATH/bin/client --hostfile /tmp/hosts --makefile Makefile-verysmall -nbthread " + str(i)
 
+			print(full_cmd_client)
 
-		
-		#p = subprocess.Popen("export TIMEFORMAT=%E; time make -f Makefile-small -j4" , shell=True, stdout=subprocess.PIPE, 
-		#							stderr=subprocess.PIPE, cwd=fmakefile)
-		#out, err = p.communicate()
+			p = subprocess.Popen(full_cmd_client , shell=True, stdout=subprocess.PIPE, 
+										stderr=subprocess.PIPE, cwd=fmakefile)
+			out, err = p.communicate()
 
-		print("time : " + str(err.split("\n")[-2]))
-		print(out)
+			#p = subprocess.Popen("export TIMEFORMAT=%E; time make -f Makefile-small -j4" , shell=True, stdout=subprocess.PIPE, 
+			#							stderr=subprocess.PIPE, cwd=fmakefile)
+			#out, err = p.communicate()
+
+			print("time : " + str(err.split("\n")[-2]))
+			print("nb thread reel : " + out.strip())
+
+			result[out.strip()].append(float(err.split("\n")[-2]))
+
+final = moyenne(result.items())
+print(final)
+file_out = open(fname, "wb")
+file_out.write("nthreads;temps\n")
+for c in final:
+	file_out.write(c[0])
+	file_out.write(";")
+	file_out.write(str(c[1]))
+	file_out.write("\n")
+file_out.close()
